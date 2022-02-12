5cb10ede1bbc3158ce73d53a95dd1eb7c4102202
Marco <89026104+MarcoooMp@users.noreply.github.com>
2022-02-12 17:00:34 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,11 @@
+var listaJogos = [
+  "https://cdn.akamai.steamstatic.com/steam/apps/42700/capsule_616x353.jpg?t=1589829053",
+
+  "https://i.ytimg.com/vi/foUaOCzfIRU/maxresdefault.jpg",
+
+  "https://www.callofduty.com/content/dam/atvi/callofduty/cod-touchui/blog/hero/mw-wz/WZ-Season-Three-Announce-TOUT.jpg"
+];
+
+for (var indice = 0; indice < listaJogos.length; indice++) {
+  document.write("<img src=" + listaJogos[indice] + ">");
+}
